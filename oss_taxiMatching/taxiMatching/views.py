27a51f2286c87923db1aa036c7b471f3ecb3a94e
choonsik <108234111+choons1k2@users.noreply.github.com>
2023-05-27 16:26:16 +0900

    Update views.py
    signup & login implementation

--- oss_taxiMatching/taxiMatching/views.py
+++ oss_taxiMatching/taxiMatching/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+#로그인
 
-# Create your views here.
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # 로그인 후 이동할 페이지를 지정합니다.
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+#회원가입
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
