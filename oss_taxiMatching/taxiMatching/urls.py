146d16dc2cd6fc266af5230ef9248970d77f3f61
choonsik <108234111+choons1k2@users.noreply.github.com>
2023-05-27 16:38:53 +0900

    Create urls.py
    app url mapping

---
+++ oss_taxiMatching/taxiMatching/urls.py
@@ -0,0 +1,9 @@
+# app - urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+]
